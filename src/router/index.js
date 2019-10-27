429f15b1a97e556ba4e50ec245d6724c9e301670
PH <phastings@openmobo.com>
2019-10-27 17:17:50 -0400

    removed helloWorld

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Cats from '../views/Cats.vue'
+import Dogs from '../views/Dogs.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +19,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/cats',
+    name: 'cats',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Cats
+  },
+  {
+    path: '/dogs',
+    name: 'dogs',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Dogs
   }
 ]
 
